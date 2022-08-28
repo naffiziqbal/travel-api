ff560ee06b7ecd1be67096f12fa24041071a157d
naffiziqbal <f.nafiziqbal@gmail.com>
2022-08-28 10:08:04 +0600

    detail

--- api.js
+++ api.js
@@ -8,22 +8,40 @@ const loadCountry =()=> {
 
 
 const displayCountry = countries => {
-    console.log(countries);
+    console.log(countries[0]);
 
     const displayCountryDiv = document.getElementById("display-country");
     for(const country of countries){
          const countryDiv = document.createElement('div');
          countryDiv.innerHTML = `
          <h3>Country Name : ${country.name.common}</h3>
-         <p>Country Capital : ${country.capital}</p>
-         <p>Country Continant : ${country.continents}</p>
-        <p>Country currencies: ${country.currencies ? country.currencies.name : 'no Currencies'}</p>
-         <p>Country population : ${country.populations}</p>
-         <p>Country Languages : ${country.languages}</p>
+         <button onclick = "loadDetails('${country.cca2}')">Details</button>
          `
          countryDiv.classList.add('display-country');
          displayCountryDiv.appendChild(countryDiv)
     }
 }
 
+const loadDetails = code => {
+    const url = `https://restcountries.com/v3.1/alpha/${code}`
+    fetch(url)
+    .then(res => res.json())
+    .then(data => displayCountryDetail(data[0]))
+}
+
+
+const displayCountryDetail = country =>{
+    const countryDetails = document.getElementById('country-details');
+    // const countryDetailDiv = document.createElement('div')
+    countryDetails.innerHTML = `
+    <h4> Country Name: ${country.name}</h4>
+    <P> Country Capital : ${country.capital}</P>
+    <P> Country Continent : ${country.continents}</P>
+    <P> Country Flag : ${country.flags.png}</P>
+    <P> Country Language : ${country.languages[0]}</P>
+    `;
+
+    // countryDetails.appendChild(countryDetailDiv)
+
+}
 loadCountry()
